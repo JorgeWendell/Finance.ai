cf29163cd10e266427c0c0b997a9c47a2e50e695
JorgeWendell <141657061+JorgeWendell@users.noreply.github.com>
2024-11-08 11:17:55 -0300

    fix: where id

--- app/_actions/add-transaction/index.ts
+++ app/_actions/add-transaction/index.ts
@@ -27,11 +27,12 @@ export const UpsertTransaction = async (params: AddTransationParams) => {
     throw new Error("Unauthorized");
   }
   await db.transaction.upsert({
-    where: {
-      id: params.id,
-    },
+    
     update: { ...params, userId },
     create: { ...params, userId },
+    where: {
+      id: params.id ?? "",
+    },
   });
   revalidatePath("/transactions");
 };
