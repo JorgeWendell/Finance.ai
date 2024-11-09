46e4bd6245eac809d990759549dc8ae19689958e
JorgeWendell <141657061+JorgeWendell@users.noreply.github.com>
2024-11-09 17:45:17 -0300

    feat: add summary Cards

---
+++ app/(home)/_components/time-select.tsx
@@ -0,0 +1,53 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/_components/ui/select";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+const MONTH_OPTIONS = [
+  { value: "01", label: "Janeiro" },
+  { value: "02", label: "Fevereiro" },
+  { value: "03", label: "Março" },
+  { value: "04", label: "Abril" },
+  { value: "05", label: "Maio" },
+  { value: "06", label: "Junho" },
+  { value: "07", label: "Julho" },
+  { value: "08", label: "Agosto" },
+  { value: "09", label: "Setembro" },
+  { value: "10", label: "Outubro" },
+  { value: "11", label: "Novembro" },
+  { value: "12", label: "Dezembro" },
+];
+
+const TimeSelect = () => {
+  const { push } = useRouter();
+  const serchParams = useSearchParams();
+  const month = serchParams.get("month");
+  const handleMounthChange = (month: string) => {
+    push(`/?month=${month}`);
+  };
+  return (
+    <Select
+      onValueChange={(value) => handleMounthChange(value)}
+      defaultValue={month ?? ""}
+    >
+      <SelectTrigger className="rounded-full w-[150px]">
+        <SelectValue placeholder="Mês" />
+      </SelectTrigger>
+      <SelectContent>
+        {MONTH_OPTIONS.map((option) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default TimeSelect;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "./_components/navbar";
+import SummaryCards from "./(home)/_components/summary-cards";
+import TimeSelect from "./(home)/_components/time-select";
+import { isMatch } from "date-fns";
 
-const  Home = async () => {
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const  Home = async ({searchParams: {month}}: HomeProps ) => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-  return <Navbar />
+
+const monthIsValid = !month || !isMatch(month, "MM");
+  if (monthIsValid) {
+    redirect("?month=01");
+  }
+  return (
+  <>
+  <div className="p-6 space-y-6">
+    <Navbar />
+    <div className="flex justify-between">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <TimeSelect />
+    </div>  
+    <SummaryCards month={month} />
+  </div>
+  </>
+  )
 }
 
 export default Home;
